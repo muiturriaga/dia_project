6e5efbe3a782010ac9c45258b7fe9b7b1ec12f75
LoickCha <63468637+LoickCha@users.noreply.github.com>
2020-05-18 07:02:50 +0200

    Add files via upload

---
+++ app/Question_3_Core.py
@@ -0,0 +1,65 @@
+# Our aim is to find the best nodes to activate in order to maximize the social influence once a budget is given. However now, we do not know the activation probabilities. We can only observe the activation of edges.
+
+# Assumptions :
+# All the nodes have the same budget activation
+# The activation probabilities are not known but activation of edges are known.
+# The budget is constant over experiences.
+
+import os
+os.chdir("C:/Users/Loick/Documents/Ecole Nationale des Ponts et Chaussées/2A/Erasmus Milan/Data Analysis/dia_project-master")
+
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+from app.functions.socialNetwork import *
+from app.functions.Question_3_functions import *
+from networkx.algorithms import bipartite
+
+
+bool_affichage = True
+N_nodes = 100
+Edges_info, Nodes_info, Color_map = create_graph(['A'], ['B','C'], N_nodes, 0.02)
+
+if bool_affichage == True :
+    test = []
+    for nodes in Nodes_info[2]:
+        test.append(nodes.special_feature[0])
+    print('Nodes of type [A,B,C] : ', test.count('A'), test.count('B'), test.count('C'))
+    draw_graph(Edges_info[0], Nodes_info[2], Color_map)
+
+
+## 2. Design Algorithm.
+# Arms are our nodes. A superarm is a collection of nodes given a certain budget. Our environment is characterized by nodes and a budget. We do not know the activation probabilities of edges.
+
+T = 10
+n_experiments = 100
+lin_social_ucb_rewards_per_experiment = []
+Budget = int(N_nodes/10)
+arms_features = get_list_features(Nodes_info[2], dim = 19)
+
+List_proba_edges = []
+for edge in Edges_info[1]:
+    List_proba_edges.append(edge.proba_activation)
+
+List_special_features_nodes = [ node.special_feature for node in Nodes_info[2]]
+
+envir = SocialEnvironment(Edges_info[0], List_proba_edges, N_nodes, 'A', Budget)
+
+for e in range(0,n_experiments):
+    lin_social_ucb_learner = SocialUCBLearner(arms_features = arms_features , budget = Budget)
+    for t in range(0,T):
+        pulled_super_arm = lin_social_ucb_learner.pull_super_arm(Budget)
+        reward = calculate_reward(pulled_super_arm, List_special_features_nodes ,envir)
+# here.
+        lin_social_ucb_learner.update(pulled_super_arm, reward)
+        lin_social_ucb_rewards_per_experiment.append(lin_social_ucb_learner.collected_rewards)
+
+opt = envir.opt()
+plt.figure(0)
+plt.ylabel("Regret")
+plt.xlabel("t")
+plt.plot(np.cumsum(np.mean(opt - lin_social_ucb_rewards_per_experiment, axis = 0)), 'r')
+plt.legend(["LinUCB"])
+plt.show()
